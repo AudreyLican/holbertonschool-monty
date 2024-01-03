4077219015a9e7885c12b2f673a51782054ed5c4
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-01-03 13:24:35 +0100

    added pall function

--- print.c
+++ print.c
@@ -1,8 +1,17 @@
 #include "header.h"
 
-void pall( __attribute__((unused)) stack_t **stack, __attribute__((unused)) unsigned int line_number)
+void pall(stack_t **stack, __attribute__((unused)) unsigned int line_number)
 {
+	stack_t *cursor;
 
+	cursor = *stack;
+	if (!cursor)
+		return;
+	while (cursor)
+	{
+		printf("%d\n", cursor->n);
+		cursor = cursor->next;
+	}
 }
 void pint(__attribute__((unused)) stack_t **stack, __attribute__((unused)) unsigned int line_number)
 {
