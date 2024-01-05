903cda2e991ed2a8c19cab2c50dcb139df3a4440
AudreyLican <audrey.lican@gmail.com>
2024-01-05 10:02:35 +0100

    added function swap

--- modify.c
+++ modify.c
@@ -8,7 +8,27 @@
 void swap(__attribute__((unused)) stack_t **stack,
 __attribute__((unused)) unsigned int line_number)
 {
+	stack_t *temp;
+	GlobalMonty *Monty = getGlobalMonty();
+	int stack_height = 0, temp_num;
 
+	temp = *stack;
+	while (temp)
+	{
+		temp = temp->next;
+		stack_height++;
+	}
+	if (stack_height < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+
+		free_all_close_file(Monty->lineContent, stack, Monty->openFile);
+		exit(EXIT_FAILURE);
+	}
+	temp = *stack;
+	temp_num = temp->n;
+	temp->n = temp->next->n;
+	temp->next->n = temp_num;
 }
 /**
  * add - adds the top two elements of the stack
