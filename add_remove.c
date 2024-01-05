443ed246a05642132fe63cf3cb3af0d3fde59a46
AudreyLican <audrey.lican@gmail.com>
2024-01-05 10:25:06 +0100

    func pop "attibute ((unused))" deleted

--- add_remove.c
+++ add_remove.c
@@ -54,8 +54,7 @@ void push(stack_t **stack, unsigned int line_number)
  * @line_number: line where this pop is called
  */
 
-void pop(__attribute__((unused)) stack_t **stack,
-__attribute__((unused)) unsigned int line_number)
+void pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp;
 	GlobalMonty *Monty = getGlobalMonty();
